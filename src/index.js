dedf16c6f31b972477f1c9929787408eb88fa567
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2024-01-12 18:51:11 +0500

    refactor webpack config file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import webpackLogo from "./assets/webpack-logo.png";
 import xml from "./assets/data.xml";
 import csv from "./assets/data.csv";
 import "./styles/styles.css";
+import "./styles/less.less";
+import "./styles/scss.scss";
 
 const post = new Post("Webpack Post title", webpackLogo);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,41 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
+const isDev = process.env.NODE_ENV === "development";
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
+
+  if (!isDev) {
+    config.minimizer = [new CssMinimizerPlugin(), new TerserPlugin()];
+  }
+  return config;
+};
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+    "css-loader",
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+
+  return loaders;
+};
+
+console.log("isDev: ", isDev);
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
@@ -10,7 +44,7 @@ module.exports = {
     analytics: "./analytics.js",
   },
   output: {
-    filename: "[name].[contenthash].js",
+    filename: filename("js"),
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
@@ -20,23 +54,44 @@ module.exports = {
       "@": path.resolve(__dirname, "src"),
     },
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: "./index.html",
+      minify: {
+        collapseWhitespace: isDev ? false : true,
+      },
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
   ],
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: cssLoaders(),
+      },
+      {
+        test: /\.less$/i,
+        use: cssLoaders("less-loader"),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders("sass-loader"),
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
